e6b041c03d68d1d8c3544cfd7e919a8d7dada28b
d-cats <q56065614@gmail.com>
2018-11-06 09:39:03 +0800

    Fib(n) function added

--- recur.cc
+++ recur.cc
@@ -1,19 +1,34 @@
 #include <iostream>
 
-//TODO #1.1: add a function prototype for fib
-
-//TODO #2.1: add a function prototype for fact
 
 int main()
-{
-	//TODO: #1.2 call your fib function, print the result
-	
-	//TODO: #2.2 call your fact function, print the result
+{	//TODO: #1.2 call your fib function, print the result
+	fib(5);
 
+	//TODO: #2.2 call your fact function, print the result
+	fact(500);
 	return 0;
 }
 
+
+
 //TODO #2.3: add your recursive fib function from class
+int fib(int n)
+{
+	if (n <= 1)
+	{
+		return n;
+	}
+	else
+	{
+		return fib(n - 1) + return fib(n - 2)
+	}
+}
 
 
 //TODO #2.4: add your recursive fact function from class
+int fact(int n)
+{
+	cout << fib(n) << endl;
+	return n * 2;
+}
